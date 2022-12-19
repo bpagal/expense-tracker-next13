2ccbd93979fc1a86ab73aa9d605084b2baac765a
Brian <brianpagal@gmail.com>
2022-12-19 20:06:55 +0800

    fix: 🐛 redirect destination after logging in

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -48,7 +48,7 @@ export const getServerSideProps: GetServerSideProps = async (sspContext) => {
   if (session) {
     return {
       redirect: {
-        destination: '?page=1',
+        destination: '/?page=1',
         permanent: false,
       },
     };
