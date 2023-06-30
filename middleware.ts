610a14f36cd00064f069b5030d0df0c334523a6f
Brian <brianpagal@gmail.com>
2023-06-30 16:05:29 +0800

    refactor: 💡 upgrade middleware for latest supabase

--- middleware.ts
+++ middleware.ts
@@ -1,30 +1,27 @@
-import { createMiddlewareSupabaseClient } from '@supabase/auth-helpers-nextjs';
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs';
+import { NextResponse, NextRequest } from 'next/server';
 
 export async function middleware(req: NextRequest) {
-  // We need to create a response and hand it to the supabase client to be able to modify the response headers.
   const res = NextResponse.next();
-  // Create authenticated Supabase Client.
-  const supabaseClient = createMiddlewareSupabaseClient({ req, res });
-  // Check if we have a session
-  const {
-    data: { session },
-  } = await supabaseClient.auth.getSession();
+  const supabase = createMiddlewareClient({ req, res });
 
-  // Check auth condition
-  if (session) {
-    // Authentication successful, forward request to protected route.
-    return res;
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  // if user is signed in and the current path is / redirect the user to /account
+  if (user && req.nextUrl.pathname === '/') {
+    return NextResponse.redirect(new URL('/account', req.url));
   }
 
-  // Auth condition not met, redirect to home page.
-  const redirectUrl = req.nextUrl.clone();
-  redirectUrl.pathname = '/login';
-  // redirectUrl.searchParams.set(`redirectedFrom`, req.nextUrl.pathname);
-  return NextResponse.redirect(redirectUrl);
+  // if user is not signed in and the current path is not / redirect the user to /
+  if (!user && req.nextUrl.pathname !== '/') {
+    return NextResponse.redirect(new URL('/', req.url));
+  }
+
+  return res;
 }
 
 export const config = {
-  matcher: ['/', '/expenses/:path*'],
+  matcher: ['/', '/account'],
 };
