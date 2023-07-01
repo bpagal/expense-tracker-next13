70b68a9343f9e051d5a03f25c60993d7d34b083f
Brian <brianpagal@gmail.com>
2023-07-01 17:49:01 +0800

    fix: 🐛 middleware redirect path

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,9 +9,9 @@ export async function middleware(req: NextRequest) {
     data: { user },
   } = await supabase.auth.getUser();
 
-  // if user is signed in and the current path is / redirect the user to /account
+  // if user is signed in and the current path is / redirect the user to /expenses
   if (user && req.nextUrl.pathname === '/') {
-    return NextResponse.redirect(new URL('/account', req.url));
+    return NextResponse.redirect(new URL('/expenses', req.url));
   }
 
   // if user is not signed in and the current path is not / redirect the user to /
@@ -23,5 +23,5 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/', '/account'],
+  matcher: ['/', '/expenses'],
 };
