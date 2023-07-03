5c6618a0dab711c38dd1b274d77b7242bfb9d1c2
Brian <brianpagal@gmail.com>
2023-07-03 14:55:06 +0800

    style: 💄 fix auth form div style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import AuthForm from '../components/AuthForm';
 
 export default function Page() {
   return (
-    <div className="w-[350px] sm:w-[400px m-auto">
+    <div className="w-[350px] sm:w-[400px] m-auto">
       <AuthForm />
     </div>
   );
